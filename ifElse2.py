b02c414486167147b79c001570bc038007aa1e41
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-21 13:34:34 -0700

    Create ifElse2.py

---
+++ ifElse2.py
@@ -0,0 +1,13 @@
+a = 10
+b = 5
+c = 2
+if a > b:
+    if a > c:
+    print "a is greatest"
+    else:
+    print "c is greatest"
+elif b > c:
+    print "b is greatest"
+else:
+    print "c is greatest"
+    
