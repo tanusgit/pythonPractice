25c5cef317d12f897beef882d9fd77836f48b034
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-21 13:03:42 -0700

    Create format.py

---
+++ format.py
@@ -0,0 +1,5 @@
+print ('{1}, {0}'.format('one', 'two')) #print two one as two is in the index 1 position
+print ('{:a>10}'.format('hello')) # will print aaaaahellow as 10 charcaters will be printed as 10 is 
+#defined and hello will be printed and a will be printed before hello
+
+#for more info  https://pyformat.info/
