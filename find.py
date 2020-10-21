fc845ce92c31ba4fb455b3151f47739f7a252600
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-20 20:04:05 -0700

    Create find.py

---
+++ find.py
@@ -0,0 +1,2 @@
+s = "moon is beautiful" #finds the position of the desired string, and prints the first occurence of the character
+in = s.find("is") # returns 5 as "is" is in the fifth position and returns -1 if the character or the string is not in the string
