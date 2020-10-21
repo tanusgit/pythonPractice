4eeff7b56e7c2fe123fa6012346247a185aaf12a
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-20 19:48:35 -0700

    Create changecase.py

---
+++ changecase.py
@@ -0,0 +1,7 @@
+s = "this is a string"
+print s.islower() #returns true
+print s.isupper() #returns false
+print s.istitle() #returns 
+print s.isalpha() #check if only alphabets, cannot conatin space
+print s.isdigit() #check if digits
+print s.isalnum() #check if only alphabets or numbers
