52e51291100b6871f5c5fa0e6618f4d1dc9f7d7c
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-24 08:05:24 -0700

    Create whileLoop.py

---
+++ whileLoop.py
@@ -0,0 +1,22 @@
+i = 0
+while (i < 10):
+      print i
+      i = i + 1
+      
+      
+      
+def fib(n):
+    n1 = 1
+    n2 = 2
+    i = 0
+    while(i < n):
+        print n1
+        i = i+1
+        temp = n2
+        n2 = n1 + n2
+        n1 = temp
+        
+ fib(5)
+    
+    
+
