32ec4e6c903c91e28c992f8ddc2120de633c5f37
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-25 19:18:26 -0700

    Create range.py

---
+++ range.py
@@ -0,0 +1,6 @@
+p = [1, 3, 5, 6, 'apple']
+print p
+print len(p)
+print p[0] #the element in the first index
+print p[0:2] #starts with 0 but does not include the last index
+print p[0:4] #to print the last element we need to write 4
