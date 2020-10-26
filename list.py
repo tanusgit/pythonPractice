4aae81726ef279eca73eadad8775d0f27b31dd48
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-25 20:15:59 -0700

    Create list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+p = ['apple', 'orange', 'kiwi', 'lemon', 'mango']
+print p[4], p[-1] # will print the same element as -1 means n-1 where n is the size of the list
+i = 0
+while i < 5:
+      print p[i]
+      i = i + 1
