a6d1b8c5db6c0a6f3abef8fa4a71724605bd588f
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-10-28 19:39:43 -0700

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,7 @@
+for letter in 'Python':
+    print 'letter : ', letter
+    
+    
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:        # Second Example
+   print 'Current fruit :', fruit
